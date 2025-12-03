a52d1e0f4b6659e8fc387f10539abf67596925c5
Steven Borrelli <steve@borrelli.org>
2025-12-03 15:43:10 +0100

    remove dist files
    Signed-off-by: Steven Borrelli <steve@borrelli.org>

--- dist/examples/basic-app/function.d.ts
+++
@@ -1,5 +0,0 @@
-import { RunFunctionRequest, RunFunctionResponse, type FunctionHandler, type Logger } from "function-sdk-typescript";
-export declare class Function implements FunctionHandler {
-    RunFunction(req: RunFunctionRequest, logger?: Logger): Promise<RunFunctionResponse>;
-}
-//# sourceMappingURL=function.d.ts.map
