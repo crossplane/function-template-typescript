6c6cb5b06d6bd400b047f51b3c1c611f7748ad72
Steven Borrelli <steve@borrelli.org>
2025-12-03 15:02:40 +0100

    initial implementation
    Signed-off-by: Steven Borrelli <steve@borrelli.org>

---
+++ dist/examples/basic-app/function.d.ts
@@ -0,0 +1,5 @@
+import { RunFunctionRequest, RunFunctionResponse, type FunctionHandler, type Logger } from "function-sdk-typescript";
+export declare class Function implements FunctionHandler {
+    RunFunction(req: RunFunctionRequest, logger?: Logger): Promise<RunFunctionResponse>;
+}
+//# sourceMappingURL=function.d.ts.map
